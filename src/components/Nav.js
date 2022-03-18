ee31a28f65cbafd92dd4b0a189c8a8097497f866
mauilema <srjorgemaciel2002@gmail.com>
2022-03-18 01:20:36 -0400

    reformatted class to className for nav react component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,7 +23,17 @@ class Nav extends Component {
     body.setAttribute(dataThemeAttribute, newTheme);
   }
 
-  handleBurgerMenuClick = () => {
+  // handleBurgerMenuClick = () => {
+  //   let burger = document.querySelector('.burger')
+  //   let nav = document.querySelector('#'+burger.dataset.target)
+
+  //   burger.addEventListener('click', function () {
+  //     burger.classList.toggle('is-active')
+  //     nav.classList.toggle('is-active')
+  //   })
+  // }
+
+  handleBurgerMenuClick () {
     let burger = document.querySelector('.burger')
     let nav = document.querySelector('#'+burger.dataset.target)
 
@@ -34,60 +44,61 @@ class Nav extends Component {
   }
 
   render() {
+    console.log(this)
     return (
       <div>
-        <nav class="navbar is-black is-spaced" role="navigation" aria-label="main navigation">
-          <div class="navbar-brand">
-            {/* <a class="navbar-item" href="https://bulma.io">
+        <nav className="navbar is-black is-spaced" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            {/* <a className="navbar-item" href="https://bulma.io">
               <img src="../../public/images/Jose-Lema.png" width="75" alt="Jose Lema initials logo" />
             </a> */}
         
-            <a onClick={() => this.handleBurgerMenuClick()} role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" >
+            <div onClick={this.handleBurgerMenuClick} role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample" >
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
-            </a>
+            </div>
           </div>
   
-          <div id="navbarBasicExample" class="navbar-menu is-size-3 is-family-sans-serif">
-            <div id="nav-home" class="navbar-start">
-              <a href="#nav-home" class="navbar-item">
+          <div id="navbarBasicExample" className="navbar-menu is-size-3 is-family-sans-serif">
+            <div id="nav-home" className="navbar-start">
+              <a href="#nav-home" className="navbar-item">
                 Home
               </a>
         
-              <a href="#about" class="navbar-item">
+              <a href="#about" className="navbar-item">
                 About Me
               </a>
 
-              <a href="#skills" class="navbar-item">
+              <a href="#skills" className="navbar-item">
                 Skills
               </a>
 
-              <a href="#portfolio" class="navbar-item">
+              <a href="#portfolio" className="navbar-item">
                 Projects
               </a>
 
-              <a href="#resume" class="navbar-item">
+              <a href="#resume" className="navbar-item">
                 Experience
               </a>
 
-              <a href={file} class="navbar-item" target="blank">
+              <a href={file} className="navbar-item" target="blank">
                 Resume
               </a>
         
-              <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">
                   Let's Connect
                 </a>
         
-                <div class="navbar-dropdown is-size-4">
-                  <a class="navbar-item" href="mailto:srjorgemaciel2002@gmail.com" target="_blank">
+                <div className="navbar-dropdown is-size-4">
+                  <a className="navbar-item" href="mailto:srjorgemaciel2002@gmail.com" target="_blank">
                     Email
                   </a>
-                  <a class="navbar-item" href="https://www.linkedin.com/in/josemlema/" target="_blank">
+                  <a className="navbar-item" href="https://www.linkedin.com/in/josemlema/" target="_blank">
                     LinkedIn
                   </a>
-                  <a class="navbar-item" href="https://github.com/mauilema" target="_blank">
+                  <a className="navbar-item" href="https://github.com/mauilema" target="_blank">
                     Github
                   </a>
                 </div>
@@ -140,16 +151,16 @@ class Nav extends Component {
             </div>
 
         
-            <div class="navbar-end">
-              <div class="navbar-item">
+            <div className="navbar-end">
+              <div className="navbar-item">
               
-                <div class="buttons">
-                  <a class="button is-light" href="https://www.linkedin.com/in/josemlema/" target="_blank">
-                    <i class="fab fa-linkedin"></i>
+                <div className="buttons">
+                  <a className="button is-light" href="https://www.linkedin.com/in/josemlema/" target="_blank">
+                    <i className="fab fa-linkedin"></i>
                   </a>
 
-                  <a class="button is-light" href="https://github.com/mauilema" target="_blank">
-                    <i class="fab fa-github"></i>
+                  <a className="button is-light" href="https://github.com/mauilema" target="_blank">
+                    <i className="fab fa-github"></i>
                   </a>
                 </div>
               </div>
