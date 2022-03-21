12adc2c16994a6573d6b624f2ce4d8ba04908406
mauilema <srjorgemaciel2002@gmail.com>
2022-03-20 20:22:22 -0400

    Deploy React app to GitHub Pages 3.20.22 update

--- precache-manifest.18572618ac4086a6d6eb4f635f7612e0.js
+++ precache-manifest.18572618ac4086a6d6eb4f635f7612e0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08df5f0be66d02abdd8839b0abe3e28c",
+    "revision": "844496cbeff6a83394b259fbb1cb0be4",
     "url": "/index.html"
   },
   {
-    "revision": "98ac6d6f4d29f4e4b5e4",
+    "revision": "224ab675c5f5b6158749",
     "url": "/static/css/2.b8eac109.chunk.css"
   },
   {
-    "revision": "df33f58177bff78ebc9e",
+    "revision": "e334754fc15cb7038582",
     "url": "/static/css/main.cbe875a9.chunk.css"
   },
   {
-    "revision": "98ac6d6f4d29f4e4b5e4",
-    "url": "/static/js/2.00c04831.chunk.js"
+    "revision": "224ab675c5f5b6158749",
+    "url": "/static/js/2.8b520f96.chunk.js"
   },
   {
     "revision": "5b98f6111c3b89622102399d92ffdce5",
-    "url": "/static/js/2.00c04831.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8b520f96.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "df33f58177bff78ebc9e",
-    "url": "/static/js/main.10d64281.chunk.js"
+    "revision": "e334754fc15cb7038582",
+    "url": "/static/js/main.9a748629.chunk.js"
   },
   {
     "revision": "a2d41c56873c741b51b2",
